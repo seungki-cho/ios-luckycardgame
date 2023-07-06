640f0a94e91a2400bbae25db855d8f996d810ebf
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-06 10:55:41 +0900

    feat: Deck - 선언

--- LuckyCardGame/LuckyCardGame/ViewModel/Deck.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/Deck.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+final class Deck<FirstType: Typeable, SecondType: Typeable>: CustomStringConvertible {
+    private var deck: [LuckyCard<FirstType, SecondType>]
+    
+    init() {
+        deck = FirstType.allCases.flatMap { firstType in
+            SecondType.allCases.map { LuckyCard(firstType: firstType, secondType: $0) }
+        }
+        .shuffled()
+    }
+    
+    var description: String {
+        deck.map { $0.description }.joined(separator: ", ")
+    }
+}
