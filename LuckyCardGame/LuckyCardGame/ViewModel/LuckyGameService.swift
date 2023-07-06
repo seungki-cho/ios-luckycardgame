b2ba081e6d0dfde87f81a00fd6948cddf551363a
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-06 17:55:55 +0900

    feat: LuckyGameService - 룰, 덱, 바닥, 플레이어, 카드돌리기

--- LuckyCardGame/LuckyCardGame/ViewModel/LuckyGameService.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/LuckyGameService.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct LuckyGameService {
+    private let rule: LuckyGameRule
+    private let deck: LuckyDeck
+    private var players: [Player]
+    private var floor: [LuckyCard] = []
+    
+    init(rule: LuckyGameRule) {
+        self.rule = rule
+        self.deck = .init()
+        self.players = Array(repeating: Player(), count: rule.playerCount)
+    }
+    
+    mutating func dealCard() {
+        deck.discardCard(numbers: rule.removalNumbers)
+        (0..<rule.playerCount).forEach { index in
+            players[index].getCards(cards: (0..<rule.playerCardCount).map{ _ in deck.drawCard() })
+        }
+    }
+}
