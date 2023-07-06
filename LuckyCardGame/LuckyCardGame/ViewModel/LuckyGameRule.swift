465526ebd434e97b927017e59b31c4670a3701bf
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-06 17:55:15 +0900

    feat: LuckyGameRule - enum 게임 룰

--- LuckyCardGame/LuckyCardGame/ViewModel/LuckyGameRule.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/LuckyGameRule.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+enum LuckyGameRule: Int {
+    case threePlayer, fourPlayer, fivePlayer
+    
+    var playerCount: Int {
+        switch self {
+        case .threePlayer:
+            return 3
+        case .fourPlayer:
+            return 4
+        case .fivePlayer:
+            return 5
+        }
+    }
+    
+    var removalNumbers: [NumberType] {
+        switch self {
+        case .threePlayer:
+            return [NumberType.twelve]
+        default:
+            return []
+        }
+    }
+    
+    var playerCardCount: Int {
+        switch self {
+        case .threePlayer:
+            return 8
+        case .fourPlayer:
+            return 7
+        case .fivePlayer:
+            return 6
+        }
+    }
+    
+    var floorCardCount: Int {
+        switch self {
+        case .threePlayer:
+            return 8
+        case .fourPlayer:
+            return 9
+        case .fivePlayer:
+            return 10
+        }
+    }
+}
