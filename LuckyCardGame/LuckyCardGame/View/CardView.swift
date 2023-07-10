9bbdb1503250a3e53c49016df200c3d0ddf37365
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-10 17:22:42 +0900

    feat: CardView - 위 아래 숫자와, 동물 라벨을 포함하는 카드뷰 생성

--- LuckyCardGame/LuckyCardGame/View/CardView.swift
+++ LuckyCardGame/LuckyCardGame/View/CardView.swift
@@ -5,4 +5,66 @@
 //  Created by cho seungki on 2023/07/06.
 //
 
-import Foundation
+import UIKit
+
+class CardView: UIView {
+    //MARK: - UI Property
+    private let topNumberLabel = {
+        let label = UILabel()
+        label.font = .init(name: "Verdana", size: 13)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    private let animalLabel = {
+        let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    private let bottomNumberLabel = {
+        let label = UILabel()
+        label.font = .init(name: "Verdana", size: 13)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    //MARK: - LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureFrame()
+    }
+    
+    //MARK: - Helper
+    private func configureUI() {
+        layer.borderWidth = 1.0
+        [topNumberLabel, animalLabel, bottomNumberLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func configureFrame() {
+        topNumberLabel.frame = CGRect(x: bounds.width / 10,
+                                      y: bounds.height / 13,
+                                      width: bounds.width / 3,
+                                      height: bounds.height / 4)
+        
+        animalLabel.frame = CGRect(x: bounds.maxX / 2 - bounds.width / 6,
+                                   y: bounds.maxY / 2 - bounds.height / 6,
+                                   width: bounds.width / 3,
+                                   height: bounds.height / 4)
+
+        bottomNumberLabel.frame = CGRect(x: bounds.maxX - bounds.width / 4,
+                                         y: bounds.maxY - bounds.height / 3,
+                                         width: bounds.width / 3,
+                                         height: bounds.height / 4)
+    }
+}
