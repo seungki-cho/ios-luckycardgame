5bafb972ea78f5cb1f0b44ec07f41db59f69b822
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-11 13:16:24 +0900

    chore: DeckView - Common 부분 제거

--- LuckyCardGame/LuckyCardGame/View/DeckView.swift
+++ LuckyCardGame/LuckyCardGame/View/DeckView.swift
@@ -7,13 +7,7 @@
 
 import UIKit
 
-fileprivate enum Constant {
-    static let widthToHeightRatio = 0.54
-    static let xMargin = 5.0
-    static let yMargin = 5.0
-}
-
-final class DeckView: UIView {
+final class DeckView: CommonDeckView {
     //MARK: - UI Property
     let alphabetLabel: PaddingLabel = {
         let label = PaddingLabel(frame: .zero, padding: UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0))
@@ -21,19 +15,9 @@ final class DeckView: UIView {
         label.alpha = 0.5
         return label
     }()
-    let cardViews: [CardView] = {
-        let cardViews = (0..<9).map{ _ in CardView() }
-        cardViews.forEach { cardView in
-            cardView.isHidden = true;
-            cardView.backgroundColor = .white;
-            cardView.layer.cornerRadius = 15
-        }
-        return cardViews
-    }()
     
     //MARK: - Property
-    private var cardCount = 8
-    private let alphabet: String
+    let alphabet: String
     
     //MARK: - LifeCycle
     init(_ alphabet: String) {
@@ -47,20 +31,15 @@ final class DeckView: UIView {
         super.init(frame: frame)
         configureUI()
     }
-
+    
     required init?(coder: NSCoder) {
         alphabet = "A"
         super.init(coder: coder)
         configureUI()
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        configureFrame()
-    }
-    
     //MARK: - Helper
-    private func configureUI() {
+    override func configureUI() {
         layer.cornerRadius = 10
         backgroundColor = .systemGray5
         
@@ -70,7 +49,7 @@ final class DeckView: UIView {
         alphabetLabel.text = alphabet
     }
     
-    func configureFrame() {
+    override func configureFrame() {
         alphabetLabel.frame = CGRect(x: 10.0,
                                      y: frame.height / 4.0,
                                      width: frame.width,
@@ -80,21 +59,11 @@ final class DeckView: UIView {
         let cardWidth = frame.height * Constant.widthToHeightRatio
         let cardSpacing = (frame.width - Constant.xMargin * 2 - cardWidth * Double(cardCount)) / Double(cardCount-1)
         
-        for i in 0..<cardCount {
-            cardViews[i].frame = CGRect(x: Constant.xMargin + Double(i) * (cardSpacing + cardWidth),
-                                        y: Constant.yMargin,
-                                        width: cardWidth,
-                                        height: frame.height - Constant.yMargin * 2)
-        }
-    }
-    
-    func changeCards(_ cards: [LuckyCard]) {
-        cardCount = cards.count
-        cardViews.forEach { $0.isHidden = true }
-        zip(cards, cardViews).forEach { (card: LuckyCard, cardView: CardView) in
-            cardView.isHidden = false
-            cardView.change(number: card.numberType.description,
-                            animal: card.animalType.description)
+        cardViews.enumerated().forEach { index, cardView in
+            cardView.frame = CGRect(x: Constant.xMargin + Double(index) * (cardSpacing + cardWidth),
+                                    y: Constant.yMargin,
+                                    width: cardWidth,
+                                    height: bounds.height - Constant.yMargin * 2)
         }
     }
 }
