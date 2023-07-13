a8f91c9898dedbbc6b587561d877aa72ef923eed
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-14 02:04:55 +0900

    feat: FloorDeck - 바닥 덱을 위한 FloorDeck

--- LuckyCardGame/LuckyCardGame/ViewModel/FloorDeck.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/FloorDeck.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct FloorDeck: Deckable {
+    private(set) var cards: [LuckyCard]
+    
+    var description: String { cards.description }
+    
+    init(_ cards: [LuckyCard] = []) {
+        self.cards = cards
+    }
+    
+    func showCard(at index: Int) -> LuckyCard? {
+        guard (0..<cards.count) ~= index else { return nil }
+        return cards[index]
+    }
+    
+    mutating func sort() {
+        cards.sort()
+    }
+}
