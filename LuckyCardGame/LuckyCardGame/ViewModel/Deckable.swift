e1c0f32fc31e4a0d367ef285164ec95adffb3834
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-14 02:04:37 +0900

    feat: Deckable - Floor 와 LuckyDeck을 위한 프로토콜 생성

--- LuckyCardGame/LuckyCardGame/ViewModel/Deckable.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/Deckable.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol Deckable: CustomStringConvertible {
+    var cards: [LuckyCard] { get }
+    
+    func maximumSameCardCount() -> Int?
+    mutating func sort()
+}
+extension Deckable {
+    func maximumSameCardCount() -> Int? {
+        cards.reduce(into: [Int: Int]()) { (dict, card) in
+            dict[card.numberType.rawValue] = (dict[card.numberType.rawValue] ?? 0) + 1
+        }.values.max()
+    }
+}
