9c2f1a7bcc7200091e464f3a70175847e4a002f3
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-13 22:41:55 +0900

    refactor: LuckyCardMaker - Deck을 반환하도록 변경

--- LuckyCardGame/LuckyCardGame/ViewModel/LuckyCardMaker.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/LuckyCardMaker.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 struct LuckyCardMaker: LuckyCardMakerProtocol {
-    func makeNewDeck() -> [LuckyCard] {
-        AnimalType.allCases.flatMap { animal in
+    func makeNewDeck() -> LuckyDeck {
+        let allCards = AnimalType.allCases.flatMap { animal in
             NumberType.allCases.map { LuckyCard(animalType: animal, numberType: $0)}
         }.shuffled()
+        let newDeck = LuckyDeck()
+        newDeck.appendCards(allCards)
+        
+        return newDeck
     }
 }
