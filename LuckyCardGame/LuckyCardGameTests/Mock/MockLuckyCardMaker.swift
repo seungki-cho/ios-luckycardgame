fe595159a3eb195ef4f35bf07847ce65b40c62ca
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-13 13:46:14 +0900

    feat: MockLuckyCardMaker - 숫자만 역순으로 카드를 주는 메소드 구현

--- LuckyCardGame/LuckyCardGameTests/Mock/MockLuckyCardMaker.swift
+++ LuckyCardGame/LuckyCardGameTests/Mock/MockLuckyCardMaker.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@testable import LuckyCardGame
+
+class MockLuckyCardMaker: LuckyCardMakerProtocol {
+    func makeNewDeck() -> [LuckyCard] {
+        var deck = [LuckyCard]()
+        for number in NumberType.allCases.reversed() {
+            for animal in AnimalType.allCases {
+                deck.append(LuckyCard(animalType: animal, numberType: number))
+            }
+        }
+        return deck
+    }
+}
