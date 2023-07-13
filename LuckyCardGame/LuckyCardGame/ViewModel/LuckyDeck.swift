af26cdc57f065947cb8bf09cfe67d8667e8a3907
seungki-cho <beenzino@kookmin.ac.kr>
2023-07-14 02:07:05 +0900

    refactor: LuckyDeck - init으로만 수정하게 변경

--- LuckyCardGame/LuckyCardGame/ViewModel/LuckyDeck.swift
+++ LuckyCardGame/LuckyCardGame/ViewModel/LuckyDeck.swift
@@ -21,21 +21,7 @@ final class LuckyDeck: Deckable {
             !removalNumber.filter { card.isLike(number: $0) }.isEmpty
         }
     }
-    #warning("고쳐")
-    func drawCard() -> LuckyCard {
-        deck.removeLast()
-    }
     
-    func appendCards(_ cards: [LuckyCard]) {
-        deck.append(contentsOf: cards)
-    }
-    
-    func removeCards(where shouldBeRemoved: (LuckyCard) -> Bool) {
-        deck.removeAll { shouldBeRemoved($0) }
-    }
-    
-    func showCards() -> [LuckyCard] {
-        deck
     }
     
     func sort() {
